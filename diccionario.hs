1f7316c688a0efb5375d75f56f8d93b8c9b97972
Santiago Libonati <santiago.libonati@gmail.com>
2024-04-11 23:58:29 -0300

    agregado insert y arreglado search

--- diccionario.hs
+++ diccionario.hs
@@ -1,16 +1,39 @@
 data TTree k v = Node k (Maybe v ) (TTree k v ) (TTree k v ) (TTree k v )
                   | Leaf k v
-                  | E
+                  | E deriving Show
 
 -- devuelve el valor asociado a una clave
 search :: Ord k => [k] -> TTree k v -> Maybe v
 search _ E = Nothing
-search [] (Leaf k val) = Just val
-search [] (Node k mVal l m r) = mVal
-search (c:cs) (Leaf k val) = Nothing
-search (c:cs) (Node k mVal l m r) | c<k  = search cs l
-                                  | c>k  = search cs r
-                                  | otherwise = search cs m
+search [] _ = Nothing
+search letra@[c] (Leaf k val) | c==k = Just val
+                              | otherwise = Nothing
+search letra@[c] (Node k mVal l m r) | c==k = mVal
+                                     | c<k  = search letra l
+                                     | otherwise = search letra r 
+search str@(c:cs) (Leaf k val) = Nothing
+search str@(c:cs) (Node k mVal l m r) | c<k  = search str l
+                                      | c>k  = search str r
+                                      | otherwise = search cs m
+
+-- insert agrega un par (clave, valor) a un arbol. 
+-- Si la clave ya esta en el arbol, actualiza su valor.
+insert :: Ord k => [k] -> v -> TTree k v -> TTree k v
+insert [] x tree = tree
+insert [c] x E = Leaf c x
+insert (c:cs) x E = Node c Nothing E (insert cs x E) E
+insert [c] x (Leaf k val) | c==k = Leaf k x
+                          | c<k  = Node k (Just val) (Leaf c x) E E
+                          | otherwise = Node k (Just val) E E (Leaf c x)
+insert (c:cs) x (Leaf k val) | c==k = Node k (Just val) E (insert cs x E) E
+                             | c<k  = Node k (Just val) (Node c Nothing E (insert cs x E) E) E E
+                             | otherwise = Node k (Just val) E E (Node c Nothing E (insert cs x E) E)
+insert [c] x (Node k mval l m r) | c==k      = Node k (Just x) l m r
+                                 | c<k       = Node k mval (insert [c] x l) m r
+                                 | otherwise = Node k mval l m (insert [c] x r)
+insert (c:cs) x (Node k mval l m r) | c==k = Node k mval l (insert cs x m) r
+                                    | c<k  = Node k mval (insert (c:cs) x l) m r
+                                    | otherwise = Node k mval l m (insert (c:cs) x r)
 
 -- dado un arbol devuelve una lista ordenada con las claves del mismo
 keys :: TTree k v -> [[k]]
